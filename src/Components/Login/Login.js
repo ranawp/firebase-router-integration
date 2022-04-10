c3a9b1c0c81614c26e1564fbb502b1d4c5434eb2
ranawp <ranasaha4224#gmail.com>
2022-04-10 21:22:23 +0600

    login integretion

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import useFirebase from '../../Hooks/useFirebase';
 
 const Login = () => {
+    const { signInwithGoogle } = useFirebase();
     return (
         <div>
-            <h1>this is login page</h1>
+            <h1>Please login</h1>
+            <div style={{ margin: '20' }}>
+                <button onClick={signInwithGoogle}>Google Sign in </button>
+            </div>
+            <form>
+
+                <input type="email" placeholder='Your Email' /> <br />
+                <input type="passowrd" name='' placeholder='passowrd' /> <br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
---
+++ src/Components/Register/Register.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h3>Please Register now</h3>
+            <from>
+                <input type="email" placeholder='your name' /> <br />
+                <input type="email" placeholder='Your Email' /> <br />
+                <input type="passowrd" name='' placeholder='passowrd' /> <br />
+                <input type="submit" value="Register" />
+            </from>
+        </div>
+    );
+};
+
+export default Register;
